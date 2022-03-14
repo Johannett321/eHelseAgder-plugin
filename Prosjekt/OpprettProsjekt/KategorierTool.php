24b7af74af32f832c9df2b37b10598260e79e646
Johan <johan.svartdal30@gmail.com>
2022-03-14 12:03:35 +0100

    Delt opp ProsjektRedigeringKategorier.php til mange små filer

---
+++ Prosjekt/OpprettProsjekt/KategorierTool.php
@@ -0,0 +1,138 @@
+<?php
+function addKategorierTools() {
+    ?>
+    <script type="text/javascript">
+        function getFieldsForName(collapsibleName) {
+            switch (collapsibleName) {
+                case "cleverandorer":
+                    return ["cleverandorer_ls", "cleverandorer_ls_time"];
+                    break;
+                case "cleverandorer":
+                    return ["cleverandorer_ls", "cleverandorer_ls_time"];
+                    break;
+                case "cleverandorer":
+                    return ["cleverandorer_ls", "cleverandorer_ls_time"];
+                    break;
+                case "cleverandorer":
+                    return ["cleverandorer_ls", "cleverandorer_ls_time"];
+                    break;
+            }
+        }
+
+        /*
+        -------------------ANIMATIONS-------------------
+         */
+
+        function scrollToView(view) {
+            view.scrollIntoView({
+                behavior: "smooth",
+                block: "center",
+                inline: "nearest"
+            });
+        }
+
+        /*
+        -------------------CREATING COLLAPSIBLES-------------------
+         */
+
+        function isCategoryAlreadyAdded(categoryName, shouldAlert) {
+            for (let i = 0; i < collapsibles.children.length; i++) {
+                if (collapsibles.children[i].name == categoryName) {
+                    if (shouldAlert) {
+                        alert("Denne kategorien er allerede lagt til. Den kan ikke legges til to ganger.");
+                    }
+                    return true;
+                }else {
+                    console.log(collapsibles.children[i].name + " is not the same as " + categoryName);
+                }
+            }
+            return false;
+        }
+
+        function createCollapsibleWithTitle(titleText, name) {
+            if (isCategoryAlreadyAdded(name, true)) return null;
+
+            const collapsible = createCollapsibleWithoutTitle();
+            collapsible.name = name;
+
+            const title = document.createElement('h5');
+            title.innerText = titleText;
+
+            collapsible.appendChild(title);
+            return collapsible;
+        }
+
+        function createCollapsibleWithoutTitle() {
+            const collapsible = document.createElement('div');
+            collapsible.classList.add('collapsible');
+
+            const removeCollapsibleButton = document.createElement('button');
+            removeCollapsibleButton.type = "button";
+            removeCollapsibleButton.innerHTML = '<img src="https://www.nicepng.com/png/full/242-2425648_close-remove-delete-exit-cross-cancel-trash-comments.png" />';
+            removeCollapsibleButton.classList.add('removeCollapsibleButton');
+            $(removeCollapsibleButton).click(function (e) {
+                if (confirm("Er du sikker?") == true) {
+                    $(collapsible).animate({opacity: '0%', height: '0px'}, function (){$(collapsible).remove(); selectionOptionChanged();saveDeletedCollapsible(collapsible.name, getFieldsForName(collapsible.name));});
+                }
+            });
+
+            collapsible.appendChild(removeCollapsibleButton);
+            return collapsible;
+        }
+
+        function createMultiSaverTextField(title, name, placeholder, fieldContent, arrayWithBrothers, textboxNumber, savedLabel, localSave) {
+            if (fieldContent == null) fieldContent = "";
+
+            const container = document.createElement('div');
+            container.classList.add('textFieldContainer');
+
+            const field = document.createElement('input');
+            field.id = name;
+            field.name = name;
+            field.type = "text";
+            field.value = fieldContent;
+            if (placeholder != null) {
+                field.placeholder = placeholder;
+            }
+
+            addSpecialSaver(arrayWithBrothers,field,textboxNumber,savedLabel,localSave);
+
+            const label = document.createElement('label');
+            label.for = name;
+            label.innerText = title;
+
+            container.appendChild(label);
+            container.appendChild(field);
+            return container;
+        }
+
+        function createTextFieldWithLabel(title, name, placeholder, fieldContent, savedTextLabel) {
+            if (fieldContent == null) fieldContent = "";
+
+            const container = document.createElement('div');
+            container.classList.add('textFieldContainer');
+
+            const field = document.createElement('input');
+            field.id = name;
+            field.name = name;
+            field.type = "text";
+            field.value = fieldContent;
+            if (placeholder != null) {
+                field.placeholder = placeholder;
+            }
+
+            addTextSaver(field, savedTextLabel, name);
+
+            const label = document.createElement('label');
+            label.for = name;
+            label.innerText = title;
+
+            container.appendChild(label);
+            container.appendChild(field);
+            return container;
+        }
+
+        var lastKeypress = 0;
+    </script>
+    <?php
+}
---
+++ Prosjekt/OpprettProsjekt/ProsjektLoader.php
@@ -0,0 +1,68 @@
+<?php
+function shallWeLoadProsjekt() {
+    //Dersom man skal redigere et prosjekt
+    if (isset($_GET['editProsjektID'])) {
+        error_log("ProsjektID is set");
+        $prosjektID = $_GET['editProsjektID'];
+        $formatted_table_name = getCollapsiblesDatabaseRef();
+
+        global $wpdb;
+        $query = "SELECT * FROM " . $formatted_table_name . " WHERE prosjekt_id = " . $prosjektID;
+        $collapsiblesFound = $wpdb->get_results($query);
+        error_log("Found collapsibles: " . sizeof($collapsiblesFound));
+
+        for ($i = 0; $i < sizeof($collapsiblesFound); $i++) {
+            $innhold = $collapsiblesFound[$i]->innhold;
+            $innhold = str_replace("\r", '', $innhold);
+            $innhold = str_replace("\n", '\n', $innhold);
+
+            switch($collapsiblesFound[$i]->collapsible_type) {
+                case 1:
+                    //Custom kategori
+                    $customName = $collapsiblesFound[$i]->egendefinert_navn;
+                    $customName = str_replace("\r", '', $customName);
+                    $customName = str_replace("\n", '\n', $customName);
+
+                    ?>
+                    if (!colHasBeenDeletedLocally("cegenkategori" + (customColCounter+1))) {
+                    createCustomCatCol("<?php $customName?>", "<?php echo $innhold ?>");
+                    }
+                    <?php
+                    break;
+                case 2:
+                    //leverandører
+                    ?>
+                    if (!colHasBeenDeletedLocally("cleverandorer")) {
+                    console.log("Creating leverandorer col with content: <?php echo $innhold ?>");
+                    createLeverandorerCol("<?php echo $innhold ?>");
+                    }
+                    <?php
+                    break;
+                case 3:
+                    //Prosjekt-team
+                    ?>
+                    if (!colHasBeenDeletedLocally("cprosjektteam")) {
+                    createProsjektTeamCol("<?php echo $innhold ?>");
+                    }
+                    <?php
+                    break;
+                case 4:
+                    //Mer informasjon om prosjektet
+                    ?>
+                    if (!colHasBeenDeletedLocally("cmerinfo")) {
+                    createMerInfoCol("<?php echo $innhold ?>");
+                    }
+                    <?php
+                    break;
+                case 5:
+                    //Milepæler
+                    ?>
+                    if (!colHasBeenDeletedLocally("cmilepaeler")) {
+                    createMilepaelerCol("<?php echo $innhold ?>");
+                    }
+                    <?php
+                    break;
+            }
+        }
+    }
+}
