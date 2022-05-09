bb53c1bdb786fa14779390db5527a2700b757904
johansvartdal <johan.svartdal30@gmail.com>
2022-05-09 13:48:11 +0200

    CustomCat upload files

--- Prosjekt/OpprettProsjekt/Collapsibles/CustomCatCol.php
+++ Prosjekt/OpprettProsjekt/Collapsibles/CustomCatCol.php
@@ -31,6 +31,8 @@ function addCustomCatCol() {
             });
             collapsible.appendChild(field);
 
+            collapsible.appendChild(getImageUploadButton());
+
             const textField = document.createElement('textarea');
             textField.name = "cvcustomdesc" + customColCounter;
             textField.value = innhold;
@@ -40,9 +42,60 @@ function addCustomCatCol() {
             addTextSaver(field, savedTextInfo, "c" + field.id + "_ls");
             addTextSaver(textField, savedTextInfo, "c" + textField.name + "_ls");
             collapsible.appendChild(savedTextInfo);
-
             collapsibles.appendChild(collapsible);
             scrollToView(collapsible);
+        }
+
+        function getImageUploadButton() {
+            let holder = document.createElement('div');
+            holder.classList.add('uploadPhoto');
+            holder.id = "customCol" + customColCounter + "_uploadPhotoButton";
+
+            let fakeButton = document.createElement('div');
+            fakeButton.classList.add('lastOppBildeKnapp');
+
+            let fakeButtonText = document.createElement('h5');
+            fakeButtonText.innerText = "Last opp bilde";
+
+            let fakeButtonIcon = document.createElement('i');
+            fakeButtonIcon.classList.add('material-icons');
+            fakeButtonIcon.innerText = "upload";
+
+            fakeButton.appendChild(fakeButtonText);
+            fakeButton.appendChild(fakeButtonIcon);
+
+            holder.appendChild(fakeButton);
+
+            let customCatImage = document.createElement('input');
+            customCatImage.type = "file";
+            customCatImage.classList.add('hidden');
+            customCatImage.name = "CustomCat" + customColCounter + "_image";
+            customCatImage.accept = "image/*";
+            customCatImage.id = "customCol" + customColCounter + "_actualUploadButton";
+            customCatImage.onchange=function (event) {
+                loadFile(event)
+            }
+            holder.appendChild(customCatImage);
+
+            let output = document.createElement('img');
+            output.id = "output";
+            holder.appendChild(output);
+
+            const loadFile = function(event) {
+                const output = document.getElementById('output');
+                output.src = URL.createObjectURL(event.target.files[0]);
+                output.onload = function() {
+                    URL.revokeObjectURL(output.src) // free memory
+                }
+            };
+
+            fakeButton.onclick = uploadFilePressed(1);
+
+            function uploadFilePressed(number) {
+                console.log("CLIIICKED: " + number)
+                $('#customCol1_uploadPhotoButton').click();
+            }
+            return holder;
         }
     </script>
     <?php
