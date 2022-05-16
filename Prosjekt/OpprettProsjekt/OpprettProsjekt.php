cb85e937a1df262a0cfbd27cfc86b5773c1c2fa1
johansvartdal <johan.svartdal30@gmail.com>
2022-05-16 12:23:08 +0200

    Lagt til boks som forklarer at alle felter markert med * må fylles ut

--- Prosjekt/OpprettProsjekt/OpprettProsjekt.php
+++ Prosjekt/OpprettProsjekt/OpprettProsjekt.php
@@ -59,7 +59,7 @@ function lagFelter($loadedProsjekt) {
     <form action = "kategorier<?php if (isset($_GET['editProsjektID'])) echo "?editProsjektID=" . $_GET['editProsjektID']?>" method = "post" id = "minform" enctype="multipart/form-data">
         <div class="infoBlokk">
             <i class="material-icons">info</i>
-            <h5>Denne prosessen skjer i tre steg. For å gå videre til neste steg trykker du fortsett nederst på siden.</h5>
+            <h5>Denne prosessen skjer i tre steg. For å gå videre til neste steg trykker du fortsett nederst på siden. Alle felter markert med * må fylles ut</h5>
             <br>
             <textBox class="progressBar" id="steg1">
                 <p class="stegText" id="step1">Grunnleggende info</p>
