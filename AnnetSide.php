be554a92f135a29490a09345d2180aed005bb8cd
johansvartdal <johan.svartdal30@gmail.com>
2022-05-19 14:32:43 +0200

    Gitt annet sin underside liste en div

--- AnnetSide.php
+++ AnnetSide.php
@@ -4,10 +4,16 @@ add_shortcode('sc_annet_list', 'sc_annet_list');
 function sc_annet_list() {
     global $post;
     $pages =  get_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0');
-    foreach ($pages as $page) {
-        ?>
-        <a href = "<?php echo get_permalink($page)?>"><button class = "annetsideklasse"><?php echo get_the_title($page)?></button></a>
-        <br/>
+    ?>
+    <div class = "undersiderListe">
         <?php
-    }
+        foreach ($pages as $page) {
+            ?>
+            <a href = "<?php echo get_permalink($page)?>"><button class = "annetsideklasse"><?php echo get_the_title($page)?></button></a>
+            <br/>
+            <?php
+        }
+        ?>
+    </div>
+    <?php
 }
